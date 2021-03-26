58a0bef7f216438c29a9a283d390e97ae334065d
Tabish Shaik <tfshaik2@illinois.edu>
2021-03-26 15:53:42 -0500

    Added graphing program draft

---
+++ Plot.py
@@ -0,0 +1,14 @@
+
+import numpy as numpy
+import pandas
+import matplotlib
+import matplotlib.pyplot as plt
+
+testData = np.array([1,3,54,2,4,6,234,1,6,2,3,2,64])
+time = np.linspace(0, 10, num = len(testData))
+
+plt.plot(time, testData)
+
+plt.xlabel("Time")
+plt.ylabel("Flow Rate")
+plt.show()
