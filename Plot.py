80bb1b2bcac9f7e635b7d4d1b26186808e927b28
Tabish Shaik <tshaikenedar@gmail.com>
2021-03-26 16:03:47 -0500

    Fixed import numpy

--- Plot.py
+++ Plot.py
@@ -1,17 +1,14 @@
 
 import numpy as np
-import pandas 
+import pandas
 import matplotlib
 import matplotlib.pyplot as plt
-import scipy
 
-testData = np.array([1,2,3,4,5,6,7,8,9,10,11])
-yvals = np.array([1,3,54,1,2,7,4,123,4,45,6])
+testData = np.array([1,3,54,2,4,6,234,1,6,2,3,2,64])
+time = np.linspace(0, 10, num = len(testData))
 
-plt.plot (testData, yvals )
- 
+plt.plot(time, testData)
+
 plt.xlabel("Time")
 plt.ylabel("Flow Rate")
-#plt.xlim(0,50)
-#plt.ylim(0,70)
 plt.show()
