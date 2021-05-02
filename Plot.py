20b196554f2321e1b0eee146b7a993be83c3f9f6
Tabish Shaik <tshaikenedar@gmail.com>
2021-05-02 15:33:28 -0500

    Pushing so I can switch branches

--- Plot.py
+++ Plot.py
@@ -1,11 +1,11 @@
 
 import numpy as np
 import numpy as numpy
-import pandas
+#import pandas
 import matplotlib
 import matplotlib.pyplot as plt
 
-testData = np.array([1,3,54,2,4,6,234,1,6,2,3,2,64])
+testData = np.array([1,3,4,5,54,2,4,6,23.4,1,6,2,3,13.4,2,64])
 time = np.linspace(0, 10, num = len(testData))
 
 plt.plot(time, testData)
